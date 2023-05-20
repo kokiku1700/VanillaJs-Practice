48c19235467c34f1b15260cada6a0b347a90e2e1
kokiku1700 <kokiku1700@naver.com>
2023-05-21 00:18:04 +0900

    2023/05/21

--- js/modal.js
+++ js/modal.js
@@ -1,12 +1,28 @@
 let modalBtn = document.getElementsByClassName('modalImo');
 let modal = document.getElementsByClassName('modal');
 let modalClose = document.querySelector('.modal img');
+let inputButton = document.getElementById('button');
 
+//클릭시 모달창 열림
 modalBtn[0].addEventListener('click', () => {
     modal[0].style.display = 'flex';
     document.body.style.overflow = 'hidden';
 });
+
+//클릭시 모달창 닫힘
 modalClose.addEventListener('click', () => {
     modal[0].style.display = 'none';
     document.body.style.removeProperty('overflow');
-})
+});
+
+//클릭시 input value값이 저장되는 버튼
+inputButton.addEventListener('click', () => {
+    let inputValue = document.getElementById('input').value;
+    let createUl = document.getElementById('todoUl');
+    let createLi = document.createElement('li');
+
+    createLi.innerHTML = inputValue;
+
+    createUl.appendChild(createLi);
+    document.getElementById('input').value = "";
+});
